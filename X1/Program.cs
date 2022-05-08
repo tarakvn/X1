02cbce44233b649b3cff714b2f44272f3acb8b7c
tarakavian84@gmail.com <tarakavian84@gmail.com>
2022-05-08 19:45:58 +0430

    Add project files.

---
+++ X1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("We want to get the delta");
+            Console.Write("Enter b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Enter a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Enter c: ");
+            int c = int.Parse(Console.ReadLine());
+            double x1 = (-(b) + Math.Sqrt(b * b) - 4 * a * c) / 2 * a;
+            Console.WriteLine("Delta is : " + x1);
+            Console.ReadKey();
+        }
+    }
+}
